a0ee3849ee898c6ad7a912eb55497c6fc43bb097
Sébastien LAURENT <109630378+laforge-tech@users.noreply.github.com>
2023-08-24 16:12:04 +0000

    Proto in separate package

--- grpc-rust-srv/src/main.rs
+++ grpc-rust-srv/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use tonic::{transport::Server, Request, Response, Status};
+
+use grpc_rust_api::hello::hello_service_server::{HelloService, HelloServiceServer};
+use grpc_rust_api::hello::{ HelloReply, HelloRequest};
+
+#[derive(Debug, Default)]
+pub struct MyHelloService {}
+
+#[tonic::async_trait]
+impl HelloService for MyHelloService {
+    async fn hello(
+        &self,
+        request: Request<HelloRequest>, // Accept request of type HelloRequest
+    ) -> Result<Response<HelloReply>, Status> { // Return an instance of type HelloReply
+        println!("Got a request: {:?}", request);
+
+        let reply = HelloReply {
+            message: format!("Hello {}!", request.into_inner().name).into(), 
+        };
+
+        Ok(Response::new(reply)) // Send back our formatted greeting
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let greeter = MyHelloService::default();
+
+    Server::builder()
+        .add_service(HelloServiceServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
